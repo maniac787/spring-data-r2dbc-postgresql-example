d6a1f38d03e887d165ec4c3e30b0d67c85a351aa
Roberto Chasipanta <roberto.chasipanta@globant.com>
2024-03-04 09:56:13 -0500

    update spring version Open api include

--- src/main/java/com/bezkoder/spring/r2dbc/postgresql/model/Tutorial.java
+++ src/main/java/com/bezkoder/spring/r2dbc/postgresql/model/Tutorial.java
@@ -6,12 +6,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+
+@Table("tutorial")
 public class Tutorial {
 
     @Id
--- src/main/java/com/bezkoder/spring/r2dbc/postgresql/repository/TutorialRepository.java
+++ src/main/java/com/bezkoder/spring/r2dbc/postgresql/repository/TutorialRepository.java
@@ -1,6 +1,7 @@
 package com.bezkoder.spring.r2dbc.postgresql.repository;
 
 import com.bezkoder.spring.r2dbc.postgresql.model.Tutorial;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -9,5 +10,6 @@ import reactor.core.publisher.Flux;
 public interface TutorialRepository extends R2dbcRepository<Tutorial, Integer> {
     Flux<Tutorial> findByTitleContaining(String title);
 
+    @Query("select * from tutorial where published = :isPublished")
     Flux<Tutorial> findByPublished(boolean isPublished);
 }
